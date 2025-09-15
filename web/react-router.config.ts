470f817cf89c50e11bc44833172249b6216e2746
Jemin <jemsworld@gmail.com>
2025-09-15 19:52:14 +0100

    ssg

--- web/react-router.config.ts
+++ web/react-router.config.ts
@@ -1,8 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
+  prerender: true,
   basename: "/alphabets/",
 } satisfies Config;
